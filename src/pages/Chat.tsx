d344dc5abd28d771a2cd506b2515ce93e8de7643
y-jiu <chldudwn99@gmail.com>
2023-11-04 22:29:38 +0900

    Add routes

---
+++ src/pages/Chat.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Chatting() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default Chatting;
---
+++ src/pages/Recruiting.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Recruiting() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default Recruiting;
