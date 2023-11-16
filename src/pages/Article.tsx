5f231012dd944580e25551d6cb14acd32f9532ba
hyeeeonc <hyeeeonc@gmail.com>
2023-11-16 14:07:44 +0900

    Feat: Union Thumbnail

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -71,7 +71,7 @@ const Article = () => {
       </SelectWrapper>
       <Grid templateColumns="repeat(4, 1fr)" gap={10} paddingLeft={"100px"} paddingRight={"100px"}>
         {articles.map((article: any, i: number) => (
-          <ArticleCard image={"Image needs to be changed"} name={article.unionName} />
+          <ArticleCard image={"Image needs to be changed"} name={article.unionName} category={article.unionCategory} />
         ))}
       </Grid>
     </div>
